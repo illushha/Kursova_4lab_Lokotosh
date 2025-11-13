b93a50391d830ff3516c670e60cec7ad6c5bb2a2
Локотош Ілля <lokotosh.illya@lll.kpi.ua>
2025-11-13 16:17:36 +0200

    Fix error test

--- sort.h
+++ sort.h
@@ -3,10 +3,43 @@
 #include "stringProcessor.h"
 void swap(char** a, char** b)
 {
-    // paste your solution here
+     char* tmp = *a;
+    *a = *b;
+    *b = tmp;
+
+}
+inline void quick_sort_rec(char** arr, int left, int right)
+{
+    int i = left;
+    int j = right;
+    char* pivot = arr[(left + right) / 2];
+
+    while (i <= j)
+    {
+        while (std::strcmp(arr[i], pivot) < 0)  // arr[i] < pivot
+            ++i;
+
+        while (std::strcmp(arr[j], pivot) > 0)  // arr[j] > pivot
+            --j;
+
+        if (i <= j)
+        {
+            char* tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            ++i;
+            --j;
+        }
+    }
+
+    if (left < j)
+        quick_sort_rec(arr, left, j);
+    if (i < right)
+        quick_sort_rec(arr, i, right);
 }
 void quick_sort(char** arr, size_t length)
 {
-   // paste your code here
+   if (length > 1)
+        quick_sort_rec(arr, 0, static_cast<int>(length - 1));
 }
 #endif // !SORT
